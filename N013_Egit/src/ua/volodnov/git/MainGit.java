1de66faacbea879f3cc0e3dd3fca95a28d35e301
volodnov <volodnov@gmail.com>
2015-05-03 12:48:07 +0300

    added 123

--- N013_Egit/src/ua/volodnov/git/MainGit.java
+++ N013_Egit/src/ua/volodnov/git/MainGit.java
@@ -3,7 +3,7 @@ package ua.volodnov.git;
 public class MainGit {
 
 	public static void main(String[] args) {
-		System.out.println("This is test progect to publish in git!");
+		System.out.println("This is test progect to publish in git! 123");
 
 	}
 
